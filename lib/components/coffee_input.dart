dc572c8c0ee57890e73fe208e731b671b4d7767e
andrew-surratt <5783385+andrew-surratt@users.noreply.github.com>
2023-10-28 22:35:02 -0400

    Add origin input fields (#25)
    * Add origin input fields
    
    * Cleanup: extract origin function, renaming

--- lib/components/coffee_input.dart
+++ lib/components/coffee_input.dart
@@ -11,72 +11,155 @@ import '../services/finance.dart';
 import '../utils/forms.dart';
 import 'coffees.dart';
 
-class CoffeeInput extends ConsumerWidget {
-  CoffeeInput({super.key});
+class CoffeeInput extends ConsumerStatefulWidget {
+  var originFields = [
+    (
+      origin: TextEditingController(),
+      originPercentage: TextEditingController(),
+    )
+  ];
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CoffeeInput();
+}
+
+class _CoffeeInput extends ConsumerState<CoffeeInput> {
   final _formKey = GlobalKey<FormState>();
   final name = TextEditingController();
   final tasteNotes = TextfieldTagsController();
   final cost = TextEditingController();
   final weight = TextEditingController();
+  final startingFormFieldsCount = 4;
+  final endingFormFieldsCount = 2;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    var inputForm = Padding(
-        padding: const EdgeInsets.all(40),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              buildFormFieldText(
+    var formFieldsCount = startingFormFieldsCount +
+        widget.originFields.length +
+        endingFormFieldsCount;
+
+    var inputForm = Form(
+        key: _formKey,
+        child: ListView.builder(
+          itemCount: formFieldsCount,
+          itemBuilder: (context, index) {
+            if (index == formFieldsCount - 1) {
+              return buildSubmitButton(context);
+            } else if (index == formFieldsCount - 2) {
+              return buildAddOriginButton(context);
+            } else if (index == 0) {
+              return buildFormFieldText(
                   controller: name,
                   hint: 'Coffee Name',
-                  validationText: () => 'Please enter a coffee'),
-              buildMultiTagField(
+                  validationText: () => 'Enter a coffee');
+            } else if (index == 1) {
+              return buildMultiTagField(
                   controller: tasteNotes,
                   hintText: 'Tasting notes',
-                  tagColor: theme.primaryColor),
-              buildFormFieldDouble(
-                controller: cost,
-                hint: 'Cost of beans/grounds',
-                validationText: () => 'Please enter an amount',
-              ),
-              buildFormFieldDouble(
-                controller: weight,
-                hint: 'Weight of beans/grounds (oz)',
-                validationText: () => 'Please enter an amount',
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    var costValue = double.parse(cost.value.text);
-                    var weightValue = double.parse(weight.value.text);
-                    var costPerOz =
-                        toPrecision(calculateCostPerOz(costValue, weightValue));
-                    addCoffee(CoffeeCreateReq(
-                        name: name.value.text,
-                        costPerOz: costPerOz,
-                        tastingNotes: tasteNotes.getTags ?? []));
-
-                    tasteNotes.getTags?.forEach((element) {
-                      addTastingNote(element);
-                    });
-
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const Coffees(),
-                        ));
-                  },
-                  child: const Text('Submit'),
-                ),
-              )
-            ],
-          ),
+                  tagColor: theme.primaryColor);
+            } else if (index == 2) {
+              return buildFormFieldDouble(
+                  controller: cost,
+                  hint: 'Cost of beans/grounds',
+                  validationText: () => 'Enter an amount');
+            } else if (index == 3) {
+              return buildFormFieldDouble(
+                  controller: weight,
+                  hint: 'Weight of beans/grounds (oz)',
+                  validationText: () => 'Enter an amount');
+            } else {
+              return buildOriginField(index);
+            }
+          },
         ));
-    return ScaffoldBuilder(body: inputForm);
+    return ScaffoldBuilder(
+        body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: inputForm));
+  }
+
+  Row buildOriginField(int index) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Expanded(
+        flex: 5,
+        child: buildFormFieldText(
+            controller:
+                widget.originFields[index - startingFormFieldsCount].origin,
+            hint: 'Origin',
+            validationText: () => 'Enter an origin country'),
+      ),
+      const Spacer(flex: 1),
+      Flexible(
+        flex: 2,
+        child: buildFormFieldDouble(
+            controller: widget
+                .originFields[index - startingFormFieldsCount].originPercentage,
+            hint: 'Percentage',
+            validationText: () => 'Enter a percentage 1-100'),
+      ),
+      TextButton(
+          onPressed: () {
+            setState(() {
+              widget.originFields.removeAt(index - startingFormFieldsCount);
+            });
+          },
+          child: const Icon(Icons.close))
+    ]);
+  }
+
+  Widget buildAddOriginButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            widget.originFields.add((
+              origin: TextEditingController(),
+              originPercentage: TextEditingController(),
+            ));
+          });
+        },
+        child: const Text('Add another origin'),
+      ),
+    );
+  }
+
+  Padding buildSubmitButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          var costValue = double.parse(cost.value.text);
+          var weightValue = double.parse(weight.value.text);
+          var costPerOz =
+              toPrecision(calculateCostPerOz(costValue, weightValue));
+          var origins = widget.originFields
+              .map((e) => CoffeeOrigin(
+                    origin: e.origin.text,
+                    percentage: double.parse(e.originPercentage.text),
+                  ))
+              .toList();
+
+          addCoffee(CoffeeCreateReq(
+            name: name.value.text,
+            costPerOz: costPerOz,
+            tastingNotes: tasteNotes.getTags ?? [],
+            origins: origins,
+          ));
+
+          tasteNotes.getTags?.forEach((element) {
+            addTastingNote(element);
+          });
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Coffees(),
+              ));
+        },
+        child: const Text('Submit'),
+      ),
+    );
   }
 }
