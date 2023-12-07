df1e53ad30ab2acb64112751259bf7ed9d9632ca
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2023-12-06 21:57:20 -0500

    Format coffee page. Add origins, name to review, certification icons.

---
+++ lib/providers/icons.dart
@@ -0,0 +1,11 @@
+import 'package:coffee_picker/services/storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final iconsProvider = FutureProvider.autoDispose((ref) async {
+  var ftIcon = await getStoredData('Fair_Trade_Icon.png');
+  var organicIcon = await getStoredData('USDA_Organic_Icon.png');
+  return (
+    fairTrade: ftIcon,
+    organic: organicIcon,
+  );
+});
