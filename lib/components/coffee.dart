f922d678d2251397dd3b326e1a139159838e272b
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2025-01-26 22:28:11 -0500

    Add advanced review helpers

--- lib/components/coffee.dart
+++ lib/components/coffee.dart
@@ -7,6 +7,7 @@ import 'package:coffee_picker/providers/icons.dart';
 import 'package:coffee_picker/services/auth.dart';
 import 'package:coffee_picker/utils/forms.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,9 +34,20 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
 
   late double ratingController;
   TextEditingController reviewController = TextEditingController();
+  final TextEditingController _aromaSliderValue =
+      TextEditingController(text: 5.toString());
+  final TextEditingController _aciditySliderValue =
+      TextEditingController(text: 5.toString());
+  final TextEditingController _sweetnessSliderValue =
+      TextEditingController(text: 5.toString());
+  final TextEditingController _bodySliderValue =
+      TextEditingController(text: 5.toString());
+  final TextEditingController _finishSliderValue =
+      TextEditingController(text: 5.toString());
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     ratingController = defaultRating;
     User? user = getUser();
     Future<List<Rating>> ratings = getCoffeeRatings(widget.coffee);
@@ -69,29 +81,45 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
               ],
             ),
             const Divider(height: 50, thickness: 1),
-            buildForm(buildFormFields(context, ref, widget.coffee, user)),
+            ExpansionTile(
+              title: Text('Leave a review'),
+              collapsedBackgroundColor: theme.focusColor,
+              backgroundColor: theme.focusColor,
+              collapsedShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              children: <Widget>[
+                buildForm(buildFormFields(context, ref, widget.coffee, user)),
+              ],
+            ),
+            const Divider(height: 50, thickness: 1),
             ...buildReview(reviews),
           ]);
           return ScaffoldBuilder(
             body: inputForm,
-            appBarActions: (config.value?.isComparisonChartEnabled ?? false) ? [
-              PopupMenuButton<MenuItem>(
-                onSelected: (MenuItem i) {
-                  switch (i) {
-                    case MenuItem.addToComparison:
-                      ref.read(compareCoffeesProvider).addCoffee(widget.coffee, getUser());
-                  }
-                },
-                itemBuilder: (BuildContext context) {
-                  return [
-                    const PopupMenuItem<MenuItem>(
-                      value: MenuItem.addToComparison,
-                      child: Text('Add to comparison'),
+            appBarActions: (config.value?.isComparisonChartEnabled ?? false)
+                ? [
+                    PopupMenuButton<MenuItem>(
+                      onSelected: (MenuItem i) {
+                        switch (i) {
+                          case MenuItem.addToComparison:
+                            ref
+                                .read(compareCoffeesProvider)
+                                .addCoffee(widget.coffee, getUser());
+                        }
+                      },
+                      itemBuilder: (BuildContext context) {
+                        return [
+                          const PopupMenuItem<MenuItem>(
+                            value: MenuItem.addToComparison,
+                            child: Text('Add to comparison'),
+                          ),
+                        ];
+                      },
                     ),
-                  ];
-                },
-              ),
-            ]:null,
+                  ]
+                : null,
           );
         });
   }
@@ -141,7 +169,10 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
       Text(widget.coffee.name, style: h1Style),
       Text("(\$${widget.coffee.costPerOz.toString()} / oz)"),
       Wrap(children: tastingNotesRow),
-      OriginText(origins: widget.coffee.origins, maxLines: 3,),
+      OriginText(
+        origins: widget.coffee.origins,
+        maxLines: 3,
+      ),
       Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: iconsRow,
@@ -190,6 +221,21 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
           ),
           Padding(
               padding: edgeInsets,
+              child: buildSliderReview(_aromaSliderValue, 'Aroma')),
+          Padding(
+              padding: edgeInsets,
+              child: buildSliderReview(_aciditySliderValue, 'Acidity')),
+          Padding(
+              padding: edgeInsets,
+              child: buildSliderReview(_sweetnessSliderValue, 'Sweetness')),
+          Padding(
+              padding: edgeInsets,
+              child: buildSliderReview(_bodySliderValue, 'Body')),
+          Padding(
+              padding: edgeInsets,
+              child: buildSliderReview(_finishSliderValue, 'Finish')),
+          Padding(
+              padding: edgeInsets,
               child: buildFormFieldText(
                 controller: reviewController,
                 label: "Review",
@@ -217,6 +263,11 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
             coffeeRef: coffeeData.ref,
             coffeeName: coffeeData.name,
             rating: ratingController,
+            aromaValue: double.tryParse(_aromaSliderValue.text) ?? 5,
+            acidityValue: double.tryParse(_aciditySliderValue.text) ?? 5,
+            sweetnessValue: double.tryParse(_sweetnessSliderValue.text) ?? 5,
+            bodyValue: double.tryParse(_bodySliderValue.text) ?? 5,
+            finishValue: double.tryParse(_finishSliderValue.text) ?? 5,
             review: reviewController.text,
             createdAt: Timestamp.now(),
           ),
@@ -232,6 +283,11 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
   }
 
   List<Card> buildReview(List<Rating> reviews) {
+    if (kDebugMode) {
+      print("Building reviews for ${widget.coffee.name}: ${reviews.map((r) {
+        return r.userName;
+      }).join(", ")}");
+    }
     return reviews.map((r) {
       var reviewBody = Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -253,6 +309,43 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
               color: Colors.amber,
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 10,
+            children: [
+              Chip(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                label: Text("Aroma ${r.aromaValue.round()}"),
+              ),
+              Chip(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                label: Text("Acidity ${r.acidityValue.round()}"),
+              ),
+              Chip(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                label: Text("Sweetness ${r.sweetnessValue.round()}"),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 15,
+            children: [
+              Chip(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                label: Text("Body ${r.bodyValue.round()}"),
+              ),
+              Chip(
+                visualDensity: VisualDensity.compact,
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                label: Text("Finish ${r.finishValue.round()}"),
+              ),
+            ],
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 2),
             child: r.review.isNotEmpty ? Text(r.review) : const Text('...'),
@@ -266,5 +359,27 @@ class _CoffeeInfo extends ConsumerState<CoffeeInfo> {
                 children: [reviewBody],
               )));
     }).toList();
+  }
+
+  Widget buildSliderReview(TextEditingController field, String title) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(width: 90, child: Text(title)),
+        Flexible(
+            child: Slider(
+          value: double.tryParse(field.value.text) ?? 5,
+          min: 1,
+          max: 10,
+          divisions: 10,
+          label: field.value.text,
+          onChanged: (double value) {
+            setState(() {
+              field.text = value.round().toString();
+            });
+          },
+        ))
+      ],
+    );
   }
 }
